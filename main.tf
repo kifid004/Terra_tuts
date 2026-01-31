d8e384ad07d895551292e7fe74ad4655035a5926
kifid004 <dennis.arazas@gmail.com>
2026-01-31 09:11:29 +0000

    second commit

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "nextwork-unique-bucket-dennis-123" # Make sure this bucket name is globally unique by typing a long random number
+}
+
+resource "aws_s3_bucket_public_access_block" "my_bucket_public_access_block" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
